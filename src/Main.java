b43940abe373d42af42ac4eeb7aabf08ad368f76
Imhotapping <Imhotapping@mail.ru>
2025-10-17 15:21:33 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        //1 Создать переменные всех пройденных типов данных и инициализировать их значения.
+        byte a = 100;
+        short b = 21200;
+        int c = 155155;
+        long d = 2551456;
+        float x = 0.01F;
+        double y = 123456789.23;
+        char letter = 'a';
+        boolean isThis = true;
+
+        //2 Написать метод, которому в качестве параметра передается строка, обозначающая имя.
+        // Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
+        greetingByName("Roman");
+
+        //3 Написать метод, которому в качестве параметра передается целое число.
+        // Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+        System.out.println( isNegative(7) );
+        System.out.println( isNegative(-5) );
+
+        //4 Написать метод, которому в качестве параметра передается целое число,
+        // метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+        checkNumber(6);
+        checkNumber(-7);
+        checkNumber(0);
+
+        //5 Написать метод, который выводит последовательность чисел от 1000 до 0, кратных 3
+        seqNumber();
+
+    }
+
+    public static void greetingByName(String name){
+        System.out.println("Привет, " + name + "!");
+    }
+
+    public static boolean isNegative(int number){
+        return number < 0;
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("Число положительное");
+        } else if (number < 0) {
+            System.out.println("Число отрицательное");
+        } else {
+            System.out.println("Число равно нулю");
+        }
+    }
+
+    public static void seqNumber(){
+
+        int i = 1000;
+        while (i > 0) {
+            if (i % 3 == 0)
+                System.out.println(i);
+            i--;
+        }
+    }
+
 }
